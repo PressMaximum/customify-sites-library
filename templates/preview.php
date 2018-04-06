0cb0b2cca9e797860c4d914f34d9d2f0df05d968
TruongSa <shrimp2t@gmail.com>
2018-04-06 09:12:21 +0700

    Improve css

--- templates/preview.php
+++ templates/preview.php
@@ -11,7 +11,7 @@
         <div class="cs-view-ports">
             <a class="cs-preview-nav" data-action="prev" title="<?php esc_attr_e( 'Previous demo', 'customify-sites' ); ?>"  href="#"><span class="dashicons dashicons-arrow-left-alt"></span></a>
             <a class="cs-preview-nav" data-device="next" href="#"><span class="dashicons dashicons-arrow-right-alt"></span></a>
-            <a class="cs-device-view" data-device="desktop" title="<?php esc_attr_e( 'Desktop view', 'customify-sites' ); ?>" href="#"><span class="dashicons dashicons-desktop"></span></a>
+            <a class="cs-device-view current" data-device="desktop" title="<?php esc_attr_e( 'Desktop view', 'customify-sites' ); ?>" href="#"><span class="dashicons dashicons-desktop"></span></a>
             <a class="cs-device-view" data-device="tablet" title="<?php esc_attr_e( 'Tablet view', 'customify-sites' ); ?>" href="#"><span class="dashicons dashicons-tablet"></span></a>
             <a class="cs-device-view" data-device="phone" title="<?php esc_attr_e( 'Phone view', 'customify-sites' ); ?>" href="#"><span class="dashicons dashicons-smartphone"></span></a>
         </div>
