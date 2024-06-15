cd78abbc4afb9b32042977111cc2412321210075
shrimp2t <shrimp2t@gmail.com>
2024-06-15 10:36:57 +0700

    update readme

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,87 +1,87 @@
-module.exports = function( grunt ) {
-    'use strict';
-    var pkgInfo = grunt.file.readJSON('package.json');
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        // Autoprefixer.
-        postcss: {
-            options: {
-                processors: [
-                    require( 'autoprefixer' )({
-                        browsers: [
-                            '> 0.1%',
-                            'ie 8',
-                            'ie 9'
-                        ]
-                    })
-                ]
+module.exports = function (grunt) {
+  "use strict";
+  var pkgInfo = grunt.file.readJSON("package.json");
+  grunt.initConfig({
+    pkg: grunt.file.readJSON("package.json"),
+    // Autoprefixer.
+    postcss: {
+      options: {
+        processors: [
+          require("autoprefixer")({
+            browsers: ["> 0.1%", "ie 8", "ie 9"],
+          }),
+        ],
+      },
+      dist: {
+        src: "assets/css/*.css",
+      },
+    },
+
+    // SASS
+    sass: {
+      options: {
+        precision: 10,
+      },
+      dist: {
+        options: {
+          style: "expanded",
+        },
+
+        files: [
+          {
+            "assets/css/customify-sites.css":
+              "assets/sass/customify-sites.scss",
+          },
+        ],
+      },
+    },
+
+    // Minified all css files.
+    cssmin: {
+      target: {
+        files: [
+          // Customizer style
+          {
+            expand: true,
+            cwd: "assets/css",
+            src: ["*.css", "!*.min.css"],
+            dest: "assets/css",
+            ext: ".min.css",
+          },
+        ],
+      },
+    },
+
+    uglify: {
+      my_target: {
+        files: [
+          {
+            expand: true,
+            cwd: ".",
+            src: [
+              "assets/js/compatibility/*.js",
+              "!assets/js/compatibility/*.min.js",
+            ],
+            dest: ".",
+            rename: function (dst, src) {
+              return dst + "/" + src.replace(".js", ".min.js");
             },
-            dist: {
-                src: 'assets/css/*.css'
-            }
-        },
+          },
+        ],
+      },
+    },
 
-        // SASS
-        sass: {
-            options: {
-                precision: 10
-            },
-            dist: {
-                options: {
-                    style: 'expanded'
-                },
+    // Watch changes for assets.
+    watch: {
+      css: {
+        files: ["assets/sass/*.scss"],
+        tasks: [
+          //'sass',
+          "css",
+        ],
+      },
 
-                files: [
-                    {'assets/css/customify-sites.css': 'assets/sass/customify-sites.scss'}
-                ]
-            }
-        },
-
-        // Minified all css files.
-        cssmin: {
-            target: {
-                files: [
-                    // Customizer style
-                    {
-                        expand: true,
-                        cwd: 'assets/css',
-                        src: ['*.css', '!*.min.css'],
-                        dest: 'assets/css',
-                        ext: '.min.css'
-                    }
-                ]
-            }
-        },
-
-        uglify: {
-            my_target: {
-                files: [
-                    {
-                        expand: true,
-                        cwd: '.',
-                        src: ['assets/js/compatibility/*.js', '!assets/js/compatibility/*.min.js'],
-                        dest: '.',
-                        rename: function (dst, src) {
-                            return dst + '/' + src.replace('.js', '.min.js');
-                        }
-                    }
-                ]
-            }
-        },
-
-        // Watch changes for assets.
-        watch: {
-            css: {
-                files: [
-                    'assets/sass/*.scss'
-                ],
-                tasks: [
-                    //'sass',
-                    'css'
-                ]
-            }
-
-            /*,
+      /*,
             scripts: {
                 files: [
                     'assets/js/*.js'
@@ -89,167 +89,163 @@ module.exports = function( grunt ) {
                 tasks: ['uglify']
             }
             */
+    },
+
+    copy: {
+      main: {
+        options: {
+          mode: true,
         },
+        src: [
+          "**",
+          "!node_modules/**",
+          "!build/**",
+          "!css/sourcemap/**",
+          "!.git/**",
+          "!bin/**",
+          "!.gitlab-ci.yml",
+          "!bin/**",
+          "!tests/**",
+          "!phpunit.xml.dist",
+          "!*.sh",
+          "!*.map",
+          "!Gruntfile.js",
+          "!package.json",
+          "!.gitignore",
+          "!phpunit.xml",
+          "!README.md",
+          "!sass/**",
+          "!codesniffer.ruleset.xml",
+          "!vendor/**",
+          "!composer.json",
+          "!composer.lock",
+          "!package-lock.json",
+          "!phpcs.xml.dist",
+        ],
+        dest: `${pkgInfo.name}/`,
+      },
+    },
 
-        copy: {
-            main: {
-                options: {
-                    mode: true
-                },
-                src: [
-                    '**',
-                    '!node_modules/**',
-                    '!build/**',
-                    '!css/sourcemap/**',
-                    '!.git/**',
-                    '!bin/**',
-                    '!.gitlab-ci.yml',
-                    '!bin/**',
-                    '!tests/**',
-                    '!phpunit.xml.dist',
-                    '!*.sh',
-                    '!*.map',
-                    '!Gruntfile.js',
-                    '!package.json',
-                    '!.gitignore',
-                    '!phpunit.xml',
-                    '!README.md',
-                    '!sass/**',
-                    '!codesniffer.ruleset.xml',
-                    '!vendor/**',
-                    '!composer.json',
-                    '!composer.lock',
-                    '!package-lock.json',
-                    '!phpcs.xml.dist'
-                ],
-                dest: 'customify-sites/'
-            }
+    compress: {
+      main: {
+        options: {
+          archive: `${pkgInfo.name}-${pkgInfo.version}.zip`,
+          mode: "zip",
         },
+        files: [
+          {
+            src: [`./${pkgInfo.name}/**`],
+          },
+        ],
+      },
+    },
 
-        compress: {
-            main: {
-                options: {
-                    archive: 'customify-sites-' + pkgInfo.version + '.zip',
-                    mode: 'zip'
-                },
-                files: [
-                    {
-                        src: [
-                            './customify-sites/**'
-                        ]
+    clean: {
+      main: [pkgInfo.name],
+      zip: ["*.zip"],
+    },
 
-                    }
-                ]
-            }
+    makepot: {
+      target: {
+        options: {
+          domainPath: "/",
+          potFilename: "languages/customify-sites.pot",
+          potHeaders: {
+            poedit: true,
+            "x-poedit-keywordslist": true,
+          },
+          type: "wp-plugin",
+          updateTimestamp: true,
         },
+      },
+    },
 
-        clean: {
-            main: ["customify-sites"],
-            zip: ["*.zip"]
-
+    addtextdomain: {
+      options: {
+        textdomain: "customify-sites",
+      },
+      target: {
+        files: {
+          src: [
+            "*.php",
+            "**/*.php",
+            "!node_modules/**",
+            "!php-tests/**",
+            "!bin/**",
+          ],
         },
+      },
+    },
 
-        makepot: {
-            target: {
-                options: {
-                    domainPath: '/',
-                    potFilename: 'languages/customify-sites.pot',
-                    potHeaders: {
-                        poedit: true,
-                        'x-poedit-keywordslist': true
-                    },
-                    type: 'wp-plugin',
-                    updateTimestamp: true
-                }
-            }
+    bumpup: {
+      options: {
+        updateProps: {
+          pkg: "package.json",
         },
+      },
+      file: "package.json",
+    },
 
-        addtextdomain: {
-            options: {
-                textdomain: 'customify-sites'
-            },
-            target: {
-                files: {
-                    src: [
-                        '*.php',
-                        '**/*.php',
-                        '!node_modules/**',
-                        '!php-tests/**',
-                        '!bin/**',
-                    ]
-                }
-            }
-        },
+    replace: {
+      theme_main: {
+        src: ["customify-sites.php"],
+        overwrite: true,
+        replacements: [
+          {
+            from: /Version: \bv?(?:0|[1-9]\d*)\.(?:0|[1-9]\d*)\.(?:0|[1-9]\d*)(?:-[\da-z-A-Z-]+(?:\.[\da-z-A-Z-]+)*)?(?:\+[\da-z-A-Z-]+(?:\.[\da-z-A-Z-]+)*)?\b/g,
+            to: "Version: <%= pkg.version %>",
+          },
+        ],
+      },
+    },
+  });
 
-        bumpup: {
-            options: {
-                updateProps: {
-                    pkg: 'package.json'
-                }
-            },
-            file: 'package.json'
-        },
+  // Load NPM tasks to be used here
+  grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks("grunt-postcss");
+  grunt.loadNpmTasks("grunt-contrib-sass");
+  grunt.loadNpmTasks("grunt-contrib-cssmin");
+  grunt.loadNpmTasks("grunt-contrib-uglify");
 
-        replace: {
-            theme_main: {
-                src: ['customify-sites.php'],
-                overwrite: true,
-                replacements: [
-                    {
-                        from: /Version: \bv?(?:0|[1-9]\d*)\.(?:0|[1-9]\d*)\.(?:0|[1-9]\d*)(?:-[\da-z-A-Z-]+(?:\.[\da-z-A-Z-]+)*)?(?:\+[\da-z-A-Z-]+(?:\.[\da-z-A-Z-]+)*)?\b/g,
-                        to: 'Version: <%= pkg.version %>'
-                    }
-                ]
-            }
-        }
+  grunt.loadNpmTasks("grunt-contrib-concat");
+  grunt.loadNpmTasks("grunt-contrib-copy");
+  grunt.loadNpmTasks("grunt-contrib-compress");
+  grunt.loadNpmTasks("grunt-contrib-clean");
+  grunt.loadNpmTasks("grunt-wp-i18n");
+  grunt.loadNpmTasks("grunt-bumpup");
+  grunt.loadNpmTasks("grunt-text-replace");
 
-    });
+  // Register tasks
+  grunt.registerTask("default", ["watch", "css"]);
+  grunt.registerTask("css", [
+    "sass",
+    //'postcss',
+    //'cssmin'
+  ]);
 
-    // Load NPM tasks to be used here
-    grunt.loadNpmTasks( 'grunt-contrib-watch' );
-    grunt.loadNpmTasks( 'grunt-postcss' );
-    grunt.loadNpmTasks('grunt-contrib-sass');
-    grunt.loadNpmTasks( 'grunt-contrib-cssmin' );
-    grunt.loadNpmTasks('grunt-contrib-uglify');
+  // To release new version just runt 2 commands below
+  // Re create everything: grunt release --ver=<version_number>
+  // Zip file installable: grunt zipfile
 
+  grunt.registerTask("zipfile", [
+    "clean:zip",
+    "copy:main",
+    "compress:main",
+    "clean:main",
+  ]);
+  grunt.registerTask("release", function (ver) {
+    var newVersion = pkgInfo.version;
+    if (newVersion) {
+      // Replace new version
+      newVersion = newVersion ? newVersion : "patch";
+      grunt.task.run("bumpup:" + newVersion);
+      grunt.task.run("replace");
 
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-contrib-compress');
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-wp-i18n');
-    grunt.loadNpmTasks('grunt-bumpup');
-    grunt.loadNpmTasks('grunt-text-replace');
-
-    // Register tasks
-    grunt.registerTask('default', [
-        'watch',
-        'css'
-    ]);
-    grunt.registerTask( 'css', [
-        'sass'
-        //'postcss',
-        //'cssmin'
-    ]);
-
-    // To release new version just runt 2 commands below
-    // Re create everything: grunt release --ver=<version_number>
-    // Zip file installable: grunt zipfile
-
-    grunt.registerTask('zipfile', ['clean:zip', 'copy:main', 'compress:main', 'clean:main']);
-    grunt.registerTask('release', function (ver) {
-        var newVersion = pkgInfo.version;
-        if (newVersion) {
-            // Replace new version
-            newVersion = newVersion ? newVersion : 'patch';
-            grunt.task.run('bumpup:' + newVersion);
-            grunt.task.run('replace');
-
-            // i18n
-            grunt.task.run(['addtextdomain', 'makepot']);
-            // re create css file and min
-            grunt.task.run([ 'css', 'postcss' ]);
-            grunt.task.run([ 'zipfile' ]);
-        }
-    });
+      // i18n
+      grunt.task.run(["addtextdomain", "makepot"]);
+      // re create css file and min
+      grunt.task.run(["css", "postcss"]);
+      grunt.task.run(["zipfile"]);
+    }
+  });
 };
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# customify-sites
+# customify-sites-library
+
+
+
+
+# Installation from within WordPress
+
+1. Download latest version customify-sites-library-xxx.zip from https://github.com/PressMaximum/customify-sites-library/releases
+2. Visit Plugins > Add New.
+3. Click upload plugin.
+4. Select file customify-sites-library-xxx.zip and click to Install Now button.
+5. Active plugin.
+
 
